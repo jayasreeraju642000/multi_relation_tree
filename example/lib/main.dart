5b8833db46e95fd714b0c3c302906ade73b2ef5a
jayasreeraju642000 <125048048+jayasreeraju642000@users.noreply.github.com>
2024-01-01 07:20:51 +0530

    changes

---
+++ example/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:multi_relation_tree/multi_relation_tree.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text("Multi relation tree"),
+        ),
+        body: const Align(alignment: Alignment.topCenter,
+          child: MultiRelationTree())
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+
+  Column _userNode(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const Text('node'),
+            InkWell(
+              onTap: () {},
+              child: Icon(
+                Icons.add_circle_outline,
+                size: 20,
+                color: Theme.of(context).dividerColor,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          width: 250,
+          height: 30,
+          decoration: BoxDecoration(
+              color: Colors.blue[100],
+              border: Border.all(color: Theme.of(context).dividerColor),
+              borderRadius: BorderRadius.circular(8)),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.male,
+                color: Colors.blue,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(child: Text('Node name')),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/multi_relation_tree.dart
+++ lib/multi_relation_tree.dart
@@ -1,72 +1,186 @@
 library multi_relation_tree;
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-/// A Calculator.
+// class MultiRelationTree extends StatelessWidget {
+//   const MultiRelationTree({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomPaint(
+//       painter: MyPainter(),
+//     );
+//   }
+
+// }
+
+// class MyPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     const double horizontalDistance = 100.0;
+
+//     // Draw nodes
+//     drawNode(
+//         canvas, Offset(size.width / 2 - horizontalDistance, 150), "Father");
+//     drawNode(
+//         canvas, Offset(size.width / 2 + horizontalDistance, 150), "Mother");
+//     drawNode(canvas, Offset(size.width / 2, 250), "Child");
+
+//     // Draw edges
+//     drawHorizontalEdge(canvas, Offset(size.width / 2 - horizontalDistance, 150),
+//         Offset(size.width / 2 + horizontalDistance, 150));
+//     drawVerticalEdge(
+//         canvas, Offset(size.width / 2, 150), Offset(size.width / 2, 250));
+//   }
+
+//   void drawNode(Canvas canvas, Offset position, String text) {
+//     Paint nodePaint = Paint()
+//       ..color = Colors.blue
+//       ..strokeWidth = 2.0
+//       ..style = PaintingStyle.fill;
+
+//     TextPainter textPainter = TextPainter(
+//       text: TextSpan(
+//           text: text, style: TextStyle(fontSize: 12.0, color: Colors.red)),
+//       textDirection: TextDirection.ltr,
+//     )..layout();
+
+//     textPainter.paint(canvas, position - Offset(20, 25));
+//     canvas.drawRect(
+//         Rect.fromPoints(position, position + Offset(50, 25)), nodePaint);
+//   }
+
+//   void drawHorizontalEdge(Canvas canvas, Offset start, Offset end) {
+//     Paint edgePaint = Paint()
+//       ..color = Colors.black
+//       ..strokeWidth = 2.0;
+
+//     canvas.drawLine(start, end, edgePaint);
+//   }
+
+//   void drawVerticalEdge(Canvas canvas, Offset start, Offset end) {
+//     Paint edgePaint = Paint()
+//       ..color = Colors.black
+//       ..strokeWidth = 2.0;
+
+//     canvas.drawLine(start, Offset(end.dx, start.dy), edgePaint);
+//     canvas.drawLine(Offset(end.dx, start.dy), end, edgePaint);
+//   }
+
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) {
+//     return false;
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:multi_relation_tree/traverse_node.dart';
+
 class MultiRelationTree extends StatelessWidget {
+  const MultiRelationTree({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return CustomPaint(
       painter: MyPainter(),
     );
   }
-
-  /// Returns [value] plus 1.
 }
 
+enum Relation { self, married, child, parent }
+
 class MyPainter extends CustomPainter {
+  var position = const Offset(0, 50);
   @override
   void paint(Canvas canvas, Size size) {
-    final double horizontalDistance = 100.0;
-    final double verticalDistance = 50.0;
+    const double horizontalDistance = 100.0;
+
+    var menus = [
+      {
+        "id": 1,
+        "parentId": 1,
+        "name": "father",
+        'level': 0,
+        'relationID': 0,
+      },
+      {
+        "id": 2,
+        "parentId": 1,
+        "name": "mother",
+        'level': 0,
+        'relationID': 1,
+      },
+      {
+        "id": 3,
+        "parentId": 1,
+        "name": "child1",
+        'level': 1,
+        'relationID': 2,
+      },
+      {
+        "id": 4,
+        "parentId": 1,
+        "name": "child2",
+        'level': 1,
+        'relationID': 2,
+      },
+      {
+        "id": 4,
+        "parentId": 1,
+        "name": "child3",
+        'level': 1,
+        'relationID': 2,
+      },
+    ];
+    double dx = 0;
+    double dy = 50;
+    for (int i = 0; i < menus.length; i++) {
+      if (menus[i]["level"] as int == 1) {
+        dy += 50;
+      }
+      drawNode(canvas, Offset(dx, dy), menus[i]["name"] as String);
+      dx += 150;
+      
+    }
 
     // Draw nodes
-    drawNode(
-        canvas, Offset(size.width / 2 - horizontalDistance, 150), "Father");
-    drawNode(
-        canvas, Offset(size.width / 2 + horizontalDistance, 150), "Mother");
-    drawNode(canvas, Offset(size.width / 2, 250), "Child");
+    // drawNode(
+    //     canvas, Offset(size.width / 2 - horizontalDistance, 150), "Father");
+    // drawNode(
+    //     canvas, Offset(size.width / 2 + horizontalDistance, 150), "Mother");
+    // drawNode(canvas, Offset(size.width / 2 - 50 - horizontalDistance, 250),
+    //     "Child1");
+    // drawNode(canvas, Offset(size.width / 2, 250), "Child2");
+    // drawNode(canvas, Offset(size.width / 2 + 50 + horizontalDistance, 250),
+    //     "Child3");
 
-    // Draw edges
-    drawHorizontalEdge(canvas, Offset(size.width / 2 - horizontalDistance, 150),
-        Offset(size.width / 2 + horizontalDistance, 150));
-    drawVerticalEdge(
-        canvas, Offset(size.width / 2, 150), Offset(size.width / 2, 250));
-  }
-
-  void drawNode(Canvas canvas, Offset position, String text) {
-    Paint nodePaint = Paint()
-      ..color = Colors.blue
-      ..strokeWidth = 2.0
-      ..style = PaintingStyle.fill;
-
-    TextPainter textPainter = TextPainter(
-      text: TextSpan(
-          text: text, style: TextStyle(fontSize: 12.0, color: Colors.red)),
-      textDirection: TextDirection.ltr,
-    )..layout();
-
-    textPainter.paint(canvas, position - Offset(20, 25));
-    canvas.drawRect(
-        Rect.fromPoints(position, position + Offset(50, 25)), nodePaint);
-  }
-
-  void drawHorizontalEdge(Canvas canvas, Offset start, Offset end) {
-    Paint edgePaint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 2.0;
-
-    canvas.drawLine(start, end, edgePaint);
-  }
-
-  void drawVerticalEdge(Canvas canvas, Offset start, Offset end) {
-    Paint edgePaint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 2.0;
-
-    canvas.drawLine(start, Offset(end.dx, start.dy), edgePaint);
-    canvas.drawLine(Offset(end.dx, start.dy), end, edgePaint);
+    // // // Draw edges
+    // drawHorizontalEdge(
+    //     canvas,
+    //     Offset(size.width / 2 - horizontalDistance / 2, 150),
+    //     Offset(size.width / 2 + horizontalDistance / 2, 150));
+    // Paint childVerticalPaint = Paint()
+    //   ..color = Colors.red
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeWidth = 3;
+    // canvas.drawLine(
+    //     Offset((size.width / 2 - 50 - horizontalDistance), 200),
+    //     Offset((size.width / 2 - 50 - horizontalDistance), 240),
+    //     childVerticalPaint);
+    // canvas.drawLine(Offset((size.width / 2), 200),
+    //     Offset((size.width / 2), 240), childVerticalPaint);
+    // canvas.drawLine(
+    //     Offset((size.width / 2 + 50 + horizontalDistance), 200),
+    //     Offset((size.width / 2 + 50 + horizontalDistance), 240),
+    //     childVerticalPaint);
+    // canvas.drawLine(
+    //     Offset((size.width / 2 - 50 - horizontalDistance), 200),
+    //     Offset((size.width / 2 + 50 + horizontalDistance), 200),
+    //     childVerticalPaint);
+    // canvas.drawLine(Offset((size.width / 2), 200), Offset(size.width / 2, 150),
+    //     childVerticalPaint);
   }
 
   @override
